b3015fdc8f99678131cc08504f2f2eae85bf2fda
Nikz Jon <mail@nikz.in>
2017-01-09 13:38:42 +0530

    jsdom fix 2

--- api/services/AmazonService.js
+++ api/services/AmazonService.js
@@ -2,9 +2,13 @@ var jsdom = require('jsdom');
 module.exports = {
   getCurrentPrice: function(link) {
     return new Promise(function(resolve, reject) {
-      jsdom.env(link, function(err, window) {
-        var $ = require("jquery")(window);
-        resolve(parseFloat($('#priceblock_ourprice').text().trim().replace(/,/g, '')));
+      jsdom.env({
+        url: link,
+        done: function(err, window) {
+          var $ = require("jquery")(window);
+          resolve(parseFloat($('#priceblock_ourprice').text().trim().replace(/,/g, '')));
+          window.close();
+        }
       });
     });
   }
