942ad5dc896f9d1df9bf244f32d57c737dde9ce7
Nikz Jon <mail@nikz.in>
2017-01-15 05:11:59 +0530

    ssl added

--- config/env/production.js
+++ config/env/production.js
@@ -6,4 +6,9 @@ module.exports = {
   // log: {
   //   level: "silent"
   // }
+  ssl: {
+    //ca: require('fs').readFileSync(require('path').resolve(__dirname, '../ssl/my-gd-bundle.crt')),
+    key: require('fs').readFileSync(require('path').resolve(__dirname, '../../../ll_ssl/ll.key')),
+    cert: require('fs').readFileSync(require('path').resolve(__dirname, '../../../ll_ssl/ll.crt'))
+  }
 };
